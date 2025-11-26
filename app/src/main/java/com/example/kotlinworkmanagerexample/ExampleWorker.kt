31f717e33424a2b3599dc81094aa2c4c5f1e54f5
Omkar <omkar.sagvekar@soliteck.com>
2025-11-26 17:06:05 +0530

    Omkar[26-11-2025]- Implement the code for permission request for notification.

--- app/src/main/java/com/example/kotlinworkmanagerexample/ExampleWorker.kt
+++ app/src/main/java/com/example/kotlinworkmanagerexample/ExampleWorker.kt
@@ -1,7 +1,15 @@
 package com.example.kotlinworkmanagerexample
 
+import android.Manifest
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
@@ -11,7 +19,10 @@ class ExampleWorker(
     workerParams: WorkerParameters
 ) : CoroutineWorker(appContext, workerParams) {
 
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
     override suspend fun doWork(): Result {
+        showNotification("Download resumed", "Your downloading process has started again...")
+
         val inputText = inputData.getString("INPUT") ?: "No input"
 
         Log.d("ExampleWorker", "Worker running… Input = $inputText")
@@ -19,5 +30,30 @@ class ExampleWorker(
         // Return output
         val output = workDataOf("OUTPUT" to "Work finished successfully!")
         return Result.success(output)
+    }
+
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun showNotification(title: String, message: String) {
+        val channelId = "work_channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "WorkManager Notifications",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            applicationContext.getSystemService(NotificationManager::class.java)
+                .createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_menu_sort_by_size)
+            .build()
+
+        NotificationManagerCompat.from(applicationContext)
+            .notify(1001, notification)
     }
 }
--- app/src/main/java/com/example/kotlinworkmanagerexample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinworkmanagerexample/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.kotlinworkmanagerexample
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,6 +16,15 @@ import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: WorkViewModel
+
+    private val requestNotificationPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+            if (isGranted){
+                Toast.makeText(this, "Notification Permission Granted", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Notification Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +34,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                requestNotificationPermission.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
 
         viewModel = ViewModelProvider(this)[WorkViewModel::class.java]
